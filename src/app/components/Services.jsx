08d75561b09fbb7837ec2648163235a719cd5e52
Helal366 <helal56876@gmail.com>
2025-08-31 19:40:30 +0600

    Fetching error handled.

--- src/app/components/Services.jsx
+++ src/app/components/Services.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
 export default async function Services() {
-    const res=await fetch("/services.json");
+    const res=await fetch(`${process.env.NEXT_BASE_URL || "http://localhost:3000"}/services.json`);
     const data=await res.json();
   return (
-    <section className='grid grid-cols-12'>
-        <h1 className='text-4xl text-center font-semibold'>
+    <section>
+        <h1 className='text-4xl text-center font-semibold my-6'>
             Services: {data?.length}
         </h1>
+        <div className='grid grid-cols-12'>
+
+        </div>
         {
             JSON.stringify(data)
         }

9bc00e8f0194d322b5b555e44bcba8c48ab9d5a6
Helal366 <helal56876@gmail.com>
2025-09-01 21:03:32 +0600

    Lib folder created.

--- src/app/components/Services.jsx
+++ src/app/components/Services.jsx
@@ -2,25 +2,25 @@ import Image from 'next/image';
 import React from 'react'
 
 export default async function Services() {
-    const res=await fetch(`${process.env.NEXT_BASE_URL || "http://localhost:3000"}/services.json`);
-    const data=await res.json();
-  return (
-    <section>
-        <h1 className='text-4xl text-center font-semibold my-6'>
-            Services : {data?.length}
-        </h1>
-        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-            {
-                data?.map((item)=>{
-                    return (
-                        <div key={item?._id} className='overflow-hidden'>
-                            <Image className='w-full h-[200px] rounded-t-xl border border-gray-500/50' src={item?.img} width={314} height={208} alt={item?.title}/>
-                        </div>
-                    )
-                })
-            }
-        </div>
-        
-    </section>
-  )
+    const res = await fetch(`${process.env.NEXT_BASE_URL || "http://localhost:3000"}/services.json`);
+    const data = await res.json();
+    return (
+        <section>
+            <h1 className='text-4xl text-center font-semibold my-6'>
+                Services : {data?.length}
+            </h1>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    data?.map((item) => {
+                        return (
+                            <div key={item?.service_id} className='overflow-hidden '>
+                                <Image className='w-full h-[200px] rounded-t-xl border border-gray-500/50 ' src={item?.img} width={314} height={208} alt={item?.title} />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
+        </section>
+    )
 }

836bec406d75383a75c149694f805f005ca0902c
Helal366 <helal56876@gmail.com>
2025-09-02 09:51:09 +0600

    Arrow added from react icons and places it at right.

--- src/app/components/Services.jsx
+++ src/app/components/Services.jsx
@@ -1,6 +1,8 @@
+import React from 'react'
 import dbConnect, { collectionNames } from '@/lib/dbConnect';
 import Image from 'next/image';
-import React from 'react'
+import { FaArrowRight } from "react-icons/fa6";
+import Link from 'next/link';
 
 export default async function Services() {
     const servicesCollection = await dbConnect(collectionNames.servicesCollection);
@@ -16,15 +18,15 @@ export default async function Services() {
                         return (
                             <div key={item?._id} className='overflow-hidden border border-gray-700/50 rounded-t-xl p-3 bg-gray-50'>
                                 <Image className='w-full h-[200px] rounded-t-xl border border-gray-500/50 mb-3' src={item?.img} width={314} height={208} alt={item?.title} />
-                                <div>
+                                <div className='flex justify-between items-center'>
                                     <div>
                                         <h3 className='text-lg font-bold'>{item?.title}</h3>
                                         <h3 className='text-lg font-bold text-orange-500'>Price : ${item?.price}</h3>
                                     </div>
                                     <div>
-                                        <p>
-                                            
-                                        </p>
+                                        <Link href={'/'} className='font-extrabold text-orange-500'>
+                                            <FaArrowRight />
+                                        </Link>
                                     </div>
                                 </div>
                             </div>

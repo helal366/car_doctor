4045b77a42b738e50ae3e4932b1b6cdd29fcd2e1
Helal366 <helal56876@gmail.com>
2025-09-02 09:18:05 +0600

    Data presented in ui from mongodb and deleted the code for services json.

--- src/app/components/Services.jsx
+++ src/app/components/Services.jsx
@@ -3,18 +3,16 @@ import Image from 'next/image';
 import React from 'react'
 
 export default async function Services() {
-    const res = await fetch(`${process.env.NEXT_BASE_URL || "http://localhost:3000"}/services.json`);
-    const data = await res.json();
     const servicesCollection = await dbConnect("services");
     const services= await servicesCollection.find({}).toArray();
     return (
         <section>
             <h1 className='text-4xl text-center font-semibold my-6'>
-                Services : {data?.length}
+                Services : {services?.length}
             </h1>
             <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
-                    data?.map((item) => {
+                    services?.map((item) => {
                         return (
                             <div key={item?.service_id} className='overflow-hidden '>
                                 <Image className='w-full h-[200px] rounded-t-xl border border-gray-500/50 ' src={item?.img} width={314} height={208} alt={item?.title} />
@@ -23,7 +21,6 @@ export default async function Services() {
                     })
                 }
             </div>
-
         </section>
     )
 }

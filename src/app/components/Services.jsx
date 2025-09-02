281b6dfbfe986e24f27fcb01916de16e1342e2ec
Helal366 <helal56876@gmail.com>
2025-09-02 09:29:48 +0600

    Title and price are added in the cards.

--- src/app/components/Services.jsx
+++ src/app/components/Services.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default async function Services() {
     const servicesCollection = await dbConnect("services");
-    const services= await servicesCollection.find({}).toArray();
+    const services = await servicesCollection.find({}).toArray();
     return (
         <section>
             <h1 className='text-4xl text-center font-semibold my-6'>
@@ -14,8 +14,13 @@ export default async function Services() {
                 {
                     services?.map((item) => {
                         return (
-                            <div key={item?.service_id} className='overflow-hidden '>
-                                <Image className='w-full h-[200px] rounded-t-xl border border-gray-500/50 ' src={item?.img} width={314} height={208} alt={item?.title} />
+                            <div key={item?._id} className='overflow-hidden border border-gray-700/50 rounded-t-xl p-3 bg-gray-50'>
+                                <Image className='w-full h-[200px] rounded-t-xl border border-gray-500/50 mb-3' src={item?.img} width={314} height={208} alt={item?.title} />
+
+                                    <h3 className='text-lg font-bold'>{item?.title}</h3>
+                                    <h3 className='text-lg font-bold text-orange-500'>Price : ${item?.price}</h3>
+
+                                
                             </div>
                         )
                     })

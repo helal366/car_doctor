ba882a59107b6fd4db03e7d04a50dd6df7ea3289
Helal366 <helal56876@gmail.com>
2025-09-03 17:11:17 +0600

    At Service Details page, image uploaded dynamically and bg changed.

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        <section className="padding">
+        <section className="padding bg-gray-300/50">
           {children}
         </section>
       </body>
--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -9,16 +9,26 @@ export default async function ServiceDetailsPage({ params }) {
   const service = await servicesCollection.findOne({ _id: new ObjectId(id) });
   service._id=service?._id.toString()
   return (
-    <section className='py-10'>
+    <section className='py-10 '>
       <section>
         <figure className='w-full relative'>
           <Image className='w-full' src="/assets/images/checkout/checkout.png" alt="Banner" width={1137} height={300} />
-          <div className='absolute w-full h-full border-2 border-red-400 top-0 overlay-bg'>
+          <div className='absolute w-full h-full top-0 overlay-bg'>
             <div className='flex items-center ps-10 w-full h-full'>            
               <h1 className='text-white font-bold'>Service Details</h1>
             </div>
           </div>
         </figure>
+      </section>
+      <section className='py-10'>
+        <figure className='w-full'>
+        <Image className='w-full h-[300px] mb-5' src={service?.img} alt={service?.title} width={1137} height={300}/>
+        </figure>
+        <div>
+          <h1>
+            {service?.title}
+          </h1>
+        </div>
       </section>
       <div>
         Service Details for {id}

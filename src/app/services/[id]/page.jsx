bc2ad3ef16214719b861faa70e7dc8fc927f118d
Helal366 <helal56876@gmail.com>
2025-09-03 16:54:09 +0600

    Gradient background adjusted for Banner image.

--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -4,17 +4,18 @@ import Image from 'next/image';
 import React from 'react'
 
 export default async function ServiceDetailsPage({ params }) {
-  const { id } = await params;
+  const { id } = params;
   const servicesCollection = await dbConnect(collectionNames.servicesCollection);
-  const service = await servicesCollection.findOne({ _id: new ObjectId(id) })
+  const service = await servicesCollection.findOne({ _id: new ObjectId(id) });
+  service._id=service?._id.toString()
   return (
     <section className='py-10'>
       <section>
         <figure className='w-full relative'>
           <Image className='w-full' src="/assets/images/checkout/checkout.png" alt="Banner" width={1137} height={300} />
-          <div className='absolute w-full h-full border-2 border-red-400 top-0'>
+          <div className='absolute w-full h-full border-2 border-red-400 top-0 overlay-bg'>
             <div className='flex items-center ps-10 w-full h-full'>            
-              <h1 className='text-red-400'>Service Details</h1>
+              <h1 className='text-white font-bold'>Service Details</h1>
             </div>
           </div>
         </figure>

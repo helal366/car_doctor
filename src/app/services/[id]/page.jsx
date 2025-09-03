a261fa10d2782fe8e21815dd507d699eb5c20740
Helal366 <helal56876@gmail.com>
2025-09-03 09:40:47 +0600

    Image is presented in ui for ServiceDetails Page.

--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -1,10 +1,21 @@
+import dbConnect, { collectionNames } from '@/lib/dbConnect';
+import { ObjectId } from 'mongodb';
+import Image from 'next/image';
 import React from 'react'
 
 export default async function ServiceDetailsPage({params}) {
-    const p=await params
+    const {id}=await params;
+    const servicesCollection= await dbConnect(collectionNames.servicesCollection);
+    const service=await servicesCollection.findOne({_id: new ObjectId(id)})
   return (
     <section>
-        Service Details for {p?.id}
+        <section>
+          <Image className='w-full' src="/assets/images/checkout/checkout.png" alt="Banner" width={1137} height={300}/>
+        </section>
+        <div>
+          Service Details for {id}
+        </div>
+        <p>{JSON.stringify(service)}</p>
     </section>
   )
 }
